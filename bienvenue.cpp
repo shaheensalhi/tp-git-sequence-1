8b624b9f95df24247071ad3d2cf6c0d6d8210c98
Shaheen <chahin.salhi69@gmail.com>
2024-02-27 10:26:17 -0500

    modifications

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -4,3 +4,4 @@ int main()
 {
 afficherBienvenue();
 return 0;
+}
--- fonction-bienvenue.h
+++ fonction-bienvenue.h
@@ -1,6 +1,5 @@
-#include "fonction-bienvenue.h"
-#include <iostream>
-void afficherBienvenue()
-{
-std::cout << "Bienvenue le monde !" << std::endl;
-}
+#ifndef FONCTION_BIENVENUE_H
+#define FONCTION_BIENVENUE_H
+#include <string>
+void afficherBienvenue(std::string message="Bienvenue le monde !");
+#endif // FONCTION_BIENVENUE_H
